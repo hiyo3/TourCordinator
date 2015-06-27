eeb50a793b2398f005291d5e6e6ece3b5f3ee747
Kawahara Takahiro <cawahara@gmail.com>
2015-06-27 13:55:12 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TourCordinator::Application.config.secret_key_base = '58b2bd5e25414dbd693fef1f73651f1e04ef5305f94627a9096711fb017431b8dff48e488f1aa527b4401985836253d1e742f5dfcb6992b357620d9a6db30bd0'
